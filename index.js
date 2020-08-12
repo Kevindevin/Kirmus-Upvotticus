bf166a40d8fb9b23ffe3aba8258632b855d6fb79
Kevindevin <leewuzhere1994@gmail.com>
2020-08-12 11:09:28 -0400

    keeping track of thumbsup/thumbsdown

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 const KIRM_ID = process.env.KIRM;
+const KIRM_STORAGE = require('./storage.js');
 
-bot.login(TOKEN);
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+client.login(TOKEN);
+
+client.on('ready', () => {
+  KIRM_STORAGE.init();
+  console.info(`Logged in as ${client.user.tag}!`);
 });
 
-bot.on('message', msg => {
+client.on('message', msg => {
+  console.log(msg)
   if (msg.author.id === KIRM_ID) {
     msg.react('👍')
       .then(() => msg.react('👎'))
       .catch(() => console.error('One of the emojis failed to react.'));
-  } 
+  }
+  if (msg.content === '!score') {
+    KIRM_STORAGE.score()
+    .then((score) => msg.channel.send(score))
+    .catch(() => console.error('Failed to send message.'));
+  }
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  if(reaction.emoji.name === '👍') {
+    KIRM_STORAGE.upvote();
+  }
+  else if (reaction.emoji.name === '👎') {
+    KIRM_STORAGE.downvote();
+  }
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+	if(reaction.emoji.name === '👍') {
+    KIRM_STORAGE.downvote();
+  }
+  else if (reaction.emoji.name === '👎') {
+    KIRM_STORAGE.upvote();
+  }
 });
